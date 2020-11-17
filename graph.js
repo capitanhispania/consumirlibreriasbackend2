7cca5abdbbd190c2c3d33633ee4adc81c0e304f1
babo@erni-espana.es <borja.baeza@erni-espana.es>
2020-11-17 18:16:40 +0100

    terminal commit 11

--- graph.js
+++ graph.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   getSharePointList: async function(accessToken) {
     const client = getAuthenticatedClient(accessToken);
-    var SPSiteID = "auditasa.sharepoint.com/sites/myroom,f8847ec1-1c42-4f09-bd00-4dc6cbf57875,4047d743-691c-4578-9795-4c1458096e98";   // Sharepoint Site ID  
+    var SPSiteID = "auditasa.sharepoint.com/sites/myroom,701dc476-809c-4f99-a86a-a9d06ad1e086,4047d743-691c-4578-9795-4c1458096e98";   // Sharepoint Site ID  
     var ListID = "07f64111-6d12-47d0-9672-7124bdddce3b"; // List/LibraryID  
     var GraphURL = "/sites/"+ SPSiteID +"/lists/"+ ListID +"/items";  //Constructed URL  
     const user = await client
